3e7d4198334e276a9c00058ffac5be6edc4f7601
ACholberton <1103@holbertonschool.com>
2019-12-19 22:36:58 +0000

    changed structure and returned index of common set

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    a = set(set_1)
-    b = set(set_2)
-    if a & b:
-        return a & b
+    for i in set_1:
+        if i in set_2:
+            return i
