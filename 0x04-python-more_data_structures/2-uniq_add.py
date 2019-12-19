cba94af9b651870ad6eec4b051bace3bca2abf43
ACholberton <1103@holbertonschool.com>
2019-12-19 22:15:59 +0000

    implemented set and sum

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    my_list.sort()
-    add = my_list[0]
-    for i in range(len(my_list) - 1):
-        if (my_list[i] != my_list[i + 1]):
-            add += my_list[i + 1]
+    add = sum(set(my_list))
     return add
