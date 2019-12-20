d871e02b21a1de207122ae11295ecad0ba118af2
ACholberton <1103@holbertonschool.com>
2019-12-20 00:51:51 +0000

    made changes that include items()

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,5 @@
 #!usr/bin/python3
 def multiply_by_2(a_dictionary):
-    newdic = a_dictionary
-    for key in newdic:
-        newdic = newdic * 2
-    return newdic
+    dictionary = a_dictionary.copy()
+    dictionary = {key: a * 2 for key, a in dictionary.items()}
+    return dictionary
