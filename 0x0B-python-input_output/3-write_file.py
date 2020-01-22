de09bc3f891855046b24e4885c99116872fb1555
ACholberton <1103@holbertonschool.com>
2020-01-22 02:12:26 +0000

    fixed pep8 errors

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
-    with open(filename, mode ='w', encoding='utf-8') as f:
+    with open(filename, mode='w', encoding='utf-8') as f:
         file1 = f.write(text)
     return file1
