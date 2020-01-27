fe579cd3d352bbafad33a0bd513d40f2a9127bc5
ACholberton <1103@holbertonschool.com>
2020-01-27 20:47:25 +0000

    added documentation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Base Class"""
 class Base():
     __nb_objects = 0
     def __init__(self, id=None):
+        """Base class init"""
         if id is not None:
             self.id = id
         else:
