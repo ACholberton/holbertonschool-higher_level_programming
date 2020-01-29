0729b98e0359299e87eaf69881dd214b334d6335
ACholberton <1103@holbertonschool.com>
2020-01-29 00:26:53 +0000

    fixed pep8

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,6 +3,7 @@
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Class initiator"""
@@ -12,6 +13,7 @@ class Square(Rectangle):
         """Overloading the STR method"""
         return "[square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)
+
     @property
     def size(self):
         """seize method"""
