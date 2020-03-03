4ebea0c295027d391cea8c4a33ff3e0b18374cba
ACholberton <1103@holbertonschool.com>
2020-03-03 02:41:21 +0000

    fixed syntax

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,7 @@
 -- Script creates a table got second_table
 
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id, name, score)
-VALUES (1, 'John', 10),
-       (2, 'ALEX', 3),
-       (3, 'Bob', 14),
-       (4, 'George', 8);
+INSERT INTO second_table VALUES (1, 'John', 10);
+INSERT INTO second_table VALUES (2, 'ALEX', 3);
+INSERT INTO second_table VALUES (3, 'Bob', 14);
+INSERT INTO second_table VALUES (4, 'George', 8);
