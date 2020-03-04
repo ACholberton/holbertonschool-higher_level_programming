d79cf3aadec0bb560b4ad1ac82e30bb914e1e454
ACholberton <1103@holbertonschool.com>
2020-03-04 23:10:44 +0000

    syntax errors

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- This Script creates a user with all priviledges
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-SET PASSWORD FOR 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' INDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
