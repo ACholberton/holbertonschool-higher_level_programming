4641cfd56dcede06aef4a95a2216f0e0e3192518
ACholberton <1103@holbertonschool.com>
2020-03-29 21:37:39 +0000

    created a script that lists all the objects that start with n from a data base

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+this script lists all the states that start with N from
+the database hbtn_0e_0_usa
+"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    host_name = "localhost"
+    port_name = 3306
+    user_name = argv[1]
+    passwd = argv[2]
+    database = argv[3]
+
+    db = MySQLdb.connect(host=host_name, port=port_name, user=user_name,
+                         passwd=passwd, db=database)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+    rows = cur.fetchall()
+    for states in rows:
+        print(states)
+
+    cur.close()
+    db.close()
