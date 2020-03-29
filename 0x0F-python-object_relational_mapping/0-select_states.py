b8e49db075a4d1f4e9f63e62c15cd1ac77361052
ACholberton <1103@holbertonschool.com>
2020-03-29 19:38:35 +0000

    created a script that looks up an object from a database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+this script lists all the states from the database hbtn_0e_0_usa
+"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    host = "localhost"
+    port = 3306
+    user = argv[1]
+    passwd = argv[2]
+    database = argv[3]
+
+    db = MySQLdb.connect(host=host, port=port, user=user,
+                               passwd=passwd, db=database)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY states.id;")
+    rows = cur.fetchall()
+    for state in rows:
+        print(states)
+
+    cur.close()
+    db.close()
