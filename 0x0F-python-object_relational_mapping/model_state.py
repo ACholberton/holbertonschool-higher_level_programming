41db9fa382e7fdbb829248e97ca169417f7dd880
ACholberton <1103@holbertonschool.com>
2020-03-30 17:24:09 +0000

    added missing import

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ Base = declarative_base():
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import String
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
