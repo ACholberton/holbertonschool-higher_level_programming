6da99538c6bc343f3f7d7c8c557fb2cb71b6ce70
ACholberton <1103@holbertonschool.com>
2020-03-30 00:06:26 +0000

    re created task 2 script but without sql intejections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+this script takes in an argument and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    host_name = "localhost"
+    port_name = 3306
+    user_name = argv[1]
+    passwd = argv[2]
+    database = argv[3]
+    sname = argv[4]
+
+    db = MySQLdb.connect(host=host_name, port=port_name, user=user_name,
+                         passwd=passwd, db=database)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (sname, ))
+    rows = cur.fetchall()
+    for states in rows:
+        print(states)
+
+    cur.close()
+    db.close()
