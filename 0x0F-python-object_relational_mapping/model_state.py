f9e6f69fae5ea1258e3ef95080dd5123e24c9e8a
ACholberton <1103@holbertonschool.com>
2020-03-30 17:31:29 +0000

    fixed typo

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,7 +12,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class state(Base):
+class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, unique=True
                 autoincrement=True)
