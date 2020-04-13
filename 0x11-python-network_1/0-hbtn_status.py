869115c6f818f520fb2a1868afd1618c5289aff8
ACholberton <1103@holbertonschool.com>
2020-04-13 21:41:50 +0000

    added type format and decoder utf-8 to the print funcitons

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,9 +9,9 @@ def intranet_status():
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         printinfo = response.read()
         print("Body response:")
-        print("\t- type: {}".format(printinfo))
+        print("\t- type: {}".format(type(printinfo)))
         print("\t- content: {}".format(printinfo))
-        print("\t- utf8 content: {}".format(printinfo))
+        print("\t- utf8 content: {}".format(printinfo.decode('utf-8')))
 
 if __name__ == '__main__':
     intranet_status()
