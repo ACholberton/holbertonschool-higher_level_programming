e04bb20f58c02b5b8d0c04309c2832ab81679514
ACholberton <1103@holbertonschool.com>
2020-04-13 21:30:57 +0000

    created python script that fetches a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+def intranet_status():
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        printinfo = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(printinfo))
+        print("\t- content: {}".format(printinfo))
+        print("\t- utf8 content: {}".format(printinfo))
+
+if __name__ == '__main__':
+    intranet_status()
