b939e8ed291f3505af91967f66bea0177066fc0f
ACholberton <1103@holbertonschool.com>
2020-04-14 03:02:22 +0000

    removed the function and added the file guard at the top

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,13 +8,9 @@ import urllib.request
 import urllib.error
 import sys
 
-
-def urllib_errorcode():
+if __name__ == '__main__':
     try:
         with urllib.request.urlopen(sys.argv[1]) as response:
             print(response.read().decode("uft-8"))
     except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
-
-if __name__ == '__main__':
-    urllib_errorcode():
+        print("Error code: {}".format(e.reason))
