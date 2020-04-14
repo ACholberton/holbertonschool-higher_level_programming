a39b188fc4a3bdf773defd496fccfc69a6decf2d
ACholberton <1103@holbertonschool.com>
2020-04-14 00:00:45 +0000

    created a python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter, and displays the body of the response
+(decoded in utf-8)
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+
+def email_and_URL():
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({"email": email}).encode("ascii")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
+
+if __name__ == "__main__":
+    email_and_URL()
