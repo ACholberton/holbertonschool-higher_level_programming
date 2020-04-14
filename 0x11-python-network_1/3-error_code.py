e3f61d3735750f96cb8dd2b7aa544ecdc7febf09
ACholberton <1103@holbertonschool.com>
2020-04-14 01:59:06 +0000

    created a python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and displays the
+body of the response (decoded in utf-8)
+"""
+
+import urllib.request
+import urllib.error
+import sys
+
+
+def urllib_errorcode():
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode("uft-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
