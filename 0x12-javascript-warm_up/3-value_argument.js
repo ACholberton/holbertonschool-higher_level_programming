cc3ac25209ec2fbdf98f1bce653d253d5695d681
ACholberton <1103@holbertonschool.com>
2020-05-05 23:04:24 +0000

    added argv[2] to the console.log

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 // script prints the argument passed in the command line//
 
 if (process.argv[2]) {
-  console.log(process.argv);
+  console.log(process.argv[2]);
 } else {
   console.log('No argument');
 }
