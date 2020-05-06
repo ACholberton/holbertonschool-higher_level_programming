a264fc55e3c29b0109480484524e2331f6fe5f1d
ACholberton <1103@holbertonschool.com>
2020-05-06 00:29:25 +0000

    changed awesome for amazing

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script uses loop to print array//
 let i;
-const arr = ['C is Fun', 'Python is cool', 'Javascript is awesome'];
+const arr = ['C is Fun', 'Python is cool', 'Javascript is amazing'];
 for (i in arr) {
   console.log(arr[i]);
 }
