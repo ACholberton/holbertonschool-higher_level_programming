faec346e27051e907164b549ec91c00a33768f3d
ACholberton <1103@holbertonschool.com>
2020-05-06 00:31:49 +0000

    lowercase fun

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script uses loop to print array//
 let i;
-const arr = ['C is Fun', 'Python is cool', 'Javascript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (i in arr) {
   console.log(arr[i]);
 }
