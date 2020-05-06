34a5bb47bd3e1ba9c9badcd324f321e6175b6021
ACholberton <1103@holbertonschool.com>
2020-05-06 02:28:01 +0000

    created a script that returns the second largest integer

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// script searches the second biggest integer in the list of arguments//
+
+const list = process.argv.length;
+const arr = [];
+let i;
+let arg;
+
+if (list === 3 || list === 2) {
+  console.log(0);
+} else {
+  for (i = 2; i < list; i++) {
+    arg = parseInt(process.argv[i]);
+    if (!isNaN(arg)) {
+      arr.push(arg);
+    }
+  }
+  arr.pop();
+  console.log(Math.max.apply(null, arr));
+}
