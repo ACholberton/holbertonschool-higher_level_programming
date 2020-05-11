72fdaaa025efbd452d1ac078740543bfdf75231d
ACholberton <1103@holbertonschool.com>
2020-05-11 20:36:58 +0000

    updated rectangle class with constructor for height and width

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Class rectangle that defines a rectangle//
+
+class Rectangle {
+  // constructors for height and width //
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
