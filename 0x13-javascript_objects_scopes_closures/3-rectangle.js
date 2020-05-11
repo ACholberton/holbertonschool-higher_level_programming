064cf2b7c98bbe4c275afc886923b5997537c6eb
ACholberton <1103@holbertonschool.com>
2020-05-11 21:42:02 +0000

    updated rectangle class so that it prints a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// Class rectangle that defines a rectangle//
+
+class Rectangle {
+  // constructors for height and width //
+  constructor (w, h) {
+    if (parseInt(w) > 0 && parseInt(h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // method //
+  print () {
+    let x = '';
+    for (let a = 0; a < this.height; a++) {
+      x += 'X';
+    }
+    for (let b = 0; b < this.width; b++) {
+      console.log(x);
+    }
+  }
+}
+
+module.exports = Rectangle;
