9e7a6a3695b1aae0b9fb9e4b9d35110dde4857ad
ACholberton <1103@holbertonschool.com>
2020-05-15 04:57:50 +0000

    created a script that gets the name of a starwars character

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,4 @@
+// Javascript script that fetches and replaces the name of this URL: https://swapi-api.hbtn.io/api/people/5/?format=json //
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', (data) => {
+  $('DIV#character').text(data.name);
+});
