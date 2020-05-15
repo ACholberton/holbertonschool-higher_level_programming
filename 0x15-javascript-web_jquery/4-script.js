fd209184f62fdef651b049d292f6f771c6a12684
ACholberton <1103@holbertonschool.com>
2020-05-15 03:25:57 +0000

    created script that appends a new element to a list and corrected documentation in 4-script.js

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,4 +1,4 @@
-// Javascript script that toggles the class of the HTML tag HEADER to red (#FF0000) when the user clicks on the tag DIV#toggle_header// 
+// Javascript script that toggles the class of the HTML tag HEADER to red (#FF0000) when the user clicks on the tag DIV#toggle_header//
 $('DIV#toggle_header').click(function () {
   $('header').toggleClass('red green');
 });
---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,4 @@
+// scripts adds a new element to a list //
+$('DIV#add_item').click(function () {
+  $('.my_list').append('<li>Item</li>');
+});
